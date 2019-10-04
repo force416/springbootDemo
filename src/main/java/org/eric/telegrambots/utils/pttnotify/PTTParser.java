b4add8ae561363b70d8771b46ed286ba099282fc
Eric <eric@Ericde-MacBook-Air.local>
2019-10-04 23:23:25 +0800

    refine PTTParser

--- src/main/java/org/eric/telegrambots/utils/pttnotify/PTTParser.java
+++ src/main/java/org/eric/telegrambots/utils/pttnotify/PTTParser.java
@@ -33,15 +33,18 @@ public class PTTParser {
 
     private List<Post> getPostsRecursion(String board, int limitPage, int limitLike, String prevPageUrl, List<Post> acc) {
         if (limitPage == 0) {
+            // sort by id
             acc.sort(Comparator.comparingLong(Post::getId));
             return acc;
         }
 
-        String fullUrl = prevPageUrl == null ? String.format(BOARD_URL_FORMAT, board) : String.format(PTT_BASE_URL_FORMAT, prevPageUrl);
-        acc = acc == null ? new ArrayList<Post>() : acc;
+        // init acc
+        acc = (acc == null) ? new ArrayList<Post>() : acc;
 
         Document doc;
         try {
+            // 判斷要parser的url是第一頁還是之後的頁面
+            String fullUrl = prevPageUrl == null ? String.format(BOARD_URL_FORMAT, board) : String.format(PTT_BASE_URL_FORMAT, prevPageUrl);
             // 需加入cookie 跳過滿18歲的確認
             doc = Jsoup.connect(fullUrl).cookie("over18", "1").get();
         } catch (IOException e) {
@@ -54,7 +57,14 @@ public class PTTParser {
                 .attr("href");
 
         // 爬出文章的 url title like
-        Elements elements = doc.body().select("div.r-ent");
+        Elements elements = doc.body().select("div.r-list-container.action-bar-margin.bbs-screen div");
+
+        // filter 第一頁的置底公告
+        if (prevPageUrl == null) {
+            // 只拿置底線以上的element
+            elements = elements.select(".r-list-sep").prevAll();
+        }
+
         List<Post> urls = elements.stream()
                 .filter((element) -> element.select(".title a").size() > 0) // filter 被刪除的文章
                 .filter((element) -> this.getLikeFromElement(element) > limitLike) // filter 推文數
@@ -71,7 +81,8 @@ public class PTTParser {
                     post.setLike(like);
 
                     return post;
-                }).collect(Collectors.toList());
+                })
+                .collect(Collectors.toList());
 
         acc.addAll(urls);
         limitPage--;
@@ -96,7 +107,7 @@ public class PTTParser {
 
     public static void main(String[] args) {
         PTTParser pttParser = new PTTParser("Beauty");
-        List<Post> posts = pttParser.getPosts(3, 0);
+        List<Post> posts = pttParser.getPosts(1, 0);
 
         posts.stream().forEach((post) -> {
             System.out.println(post.getBoard());
